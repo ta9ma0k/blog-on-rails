3cd38bae229e6600050afb6319d999a9a807ff5c
ta9ma0k <0ismore@gmail.com>
2024-09-29 21:22:46 +0900

    登録フォームにユーザ情報を追加

--- app/app/controllers/application_controller.rb
+++ app/app/controllers/application_controller.rb
@@ -7,6 +7,6 @@ class ApplicationController < ActionController::Base
   private
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :profile, :blog_url])
   end
 end
