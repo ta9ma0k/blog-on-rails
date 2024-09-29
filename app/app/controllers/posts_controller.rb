ae63e5ce1b084da0932a0e9e648fbf4de6f65be8
ta9ma0k <0ismore@gmail.com>
2024-09-29 22:44:04 +0900

    投稿したユーザ名を表示

--- app/app/controllers/posts_controller.rb
+++ app/app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, except: %i[index]
 
   def index
-    @posts = Post.recently
+    @posts = Post.preload(:user).recently
   end
 
   def create
