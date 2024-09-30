76e1af6009c7ad57d9b44f077f098244207e1917
ta9ma0k <0ismore@gmail.com>
2024-09-30 16:48:30 +0900

    フォローのリクエストの処理を修正

--- app/app/controllers/followees_controller.rb
+++ app/app/controllers/followees_controller.rb
@@ -2,12 +2,16 @@ class FolloweesController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    user = User.find_by(name: followee_params[:name])
+    user = User.find_by(name: followee_params[:username])
     
-    render :new, status: :unprocessable_entity unless user.present? && current_user.follow(user)
+    if user.present? && current_user.follow(user)
+      redirect_to root_path
+    else
+      redirect_to root_path, alert: "フォローできませんでした"
+    end
   end
 
   private
   
-  def followee_params = params.require(:followee).permit(:name)
+  def followee_params = params.permit(:username)
 end
--- app/spec/requests/followees_spec.rb
+++ app/spec/requests/followees_spec.rb
@@ -8,14 +8,14 @@ RSpec.describe "Followees", type: :request do
 
     context 'フォローしていないユーザの場合' do
       include_examples :sign_in
-      let(:params) { { followee: { name: followed_user.name } } }
+      let(:params) { { username: followed_user.name } }
       it '指定したユーザをフォローすること' do
         expect { request }.to change { current_user.follow?(followed_user) }.from(false).to(true)
       end
     end
     context 'フォローしているユーザの場合' do
       include_examples :sign_in
-      let(:params) { { followee: { name: followed_user.name } } }
+      let(:params) { { username: followed_user.name } }
       before { create(:follow, user: current_user, followee: followed_user) }
       it 'フォローに変化がないこと' do
         expect { request }.not_to change { current_user.follows.count }
@@ -23,13 +23,13 @@ RSpec.describe "Followees", type: :request do
     end
     context '存在していないユーザ名をPOSTした場合' do
       include_examples :sign_in
-      let(:params) { { followee: { name: "nouser" } } }
+      let(:params) { { username: "nouser" } }
       it 'フォローに変化がないこと' do
         expect { request }.not_to change { current_user.follows.count }
       end
     end
     context '未認証状態の場合' do
-      let(:params) { { followee: { name: followed_user.name } } }
+      let(:params) { { username: followed_user.name } }
       it 'フォローに変化がないこと' do
         expect { request }.not_to change(Follow, :count)
       end
