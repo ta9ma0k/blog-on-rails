2f33d53d805aff796dc9096f5cbd337f526c68e8
ta9ma0k <0ismore@gmail.com>
2024-09-30 09:51:54 +0900

    存在しないユーザ名の場合404ページを表示

--- app/app/controllers/application_controller.rb
+++ app/app/controllers/application_controller.rb
@@ -4,9 +4,15 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller? 
 
+  rescue_from ActiveRecord::RecordNotFound, with: :render_404
+
   private
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :profile, :blog_url])
+  end
+
+  def render_404
+    render file: "#{Rails.root}/public/404.html", status: :not_found, layout: false
   end
 end
--- app/app/controllers/users_controller.rb
+++ app/app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def profile
     @user = User.find_by(name: params[:username])
-    render status: :not_found if @user.nil?
+    raise ActiveRecord::RecordNotFound if @user.nil?
   end
 end
