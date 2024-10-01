2277815e0a8a1ee53cd61aea10a42318b61839c9
ta9ma0k <0ismore@gmail.com>
2024-10-01 12:56:28 +0900

    サムネイル画像がある場合は表示する

--- app/app/models/post.rb
+++ app/app/models/post.rb
@@ -3,6 +3,10 @@ class Post < ApplicationRecord
   has_many :likes
   has_many :comments
 
+  has_one_attached :thumbnail do |attachable|
+    attachable.variant :thumb, resize_to_limit: [150, 150]
+  end
+
   validates :body, presence: true, length: { maximum: 140 }
 
   alias_attribute :posted_at, :created_at
