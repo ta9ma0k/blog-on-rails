01a314077915597750835e023923f7bc36da4d4b
ta9ma0k <0ismore@gmail.com>
2024-10-01 15:47:12 +0900

    ユーザ編集ページを削除

--- app/config/routes.rb
+++ app/config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations:  'users/registrations'
+  }, skip: [:registrations]
+  devise_scope :user do
+    get 'sign_up', to: 'users/registrations#new', as: :new_user
+    post 'users', to: 'users/registrations#create', as: :users
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
