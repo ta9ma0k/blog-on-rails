792e4fe9c8f1b0fb2991c72dc13561726c7f9365
ta9ma0k <0ismore@gmail.com>
2024-10-01 14:51:42 +0900

    いいねランキング通知を実装

--- app/spec/mailers/previews/notification_mailer_preview.rb
+++ app/spec/mailers/previews/notification_mailer_preview.rb
@@ -1,10 +1,19 @@
 # Preview all emails at http://localhost:3000/rails/mailers/notification_mailer
 class NotificationMailerPreview < ActionMailer::Preview
+  def initialize(args)
+    super
+    @user = User.build(name: 'postman', email: 'postman@example.com', blog_url: 'http://example.com')
+  end
   def commented
-    post_user = User.build(name: 'postman', email: 'postman@example.com', blog_url: 'http://example.com')
     comment_user = User.build(name: 'commentman', email: 'commentman@example.com', blog_url: 'http://example.com')
-    post = Post.build(body: "hello world", user: post_user)
+    post = Post.build(body: "hello world", user: @user)
     comment = Comment.build(post:, user: comment_user, body: "コメント")
     NotificationMailer.commented(comment)
+  end
+
+  def ranking
+    date = Date.new(2024, 10, 1)
+    posts = 10.times.map { Post.build(body: "post#{_1}", user: @user) }
+    NotificationMailer.ranking(@user, posts, date)
   end
 end
