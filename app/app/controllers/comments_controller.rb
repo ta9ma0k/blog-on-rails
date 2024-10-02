c49e655b5ec46bd74137a8545c67aa817c51a6db
ta9ma0k <0ismore@gmail.com>
2024-10-02 13:52:20 +0900

    コメントできなかった場合の表示を修正

--- app/app/controllers/comments_controller.rb
+++ app/app/controllers/comments_controller.rb
@@ -2,14 +2,9 @@ class CommentsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    post = Post.find_by(id: comment_params[:post_id])
+    @post = Post.find_by(id: comment_params[:post_id])
 
-    if post.present? && current_user.comment(post, comment_params[:body])
-      redirect_to redirect_path
-    else
-      redirect_to redirect_path, alert: "コメントできませんでした"
-
-    end
+    flash.now.alert = "コメントできませんでした" unless @post.present? && current_user.comment(@post, comment_params[:body])
   end
 
   private
