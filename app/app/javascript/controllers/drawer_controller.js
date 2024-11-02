5ec0ecb33acb663cddae0442da2af9341cbb8ed3
ta9ma0k <0ismore@gmail.com>
2024-11-02 22:11:48 +0900

    drawerの要素をtargetで指定して取得する

--- app/app/javascript/controllers/drawer_controller.js
+++ app/app/javascript/controllers/drawer_controller.js
@@ -2,10 +2,10 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="drawer"
 export default class extends Controller {
-  toggle(e) {
-    const detailQuery = `#${e.currentTarget.dataset.card} .detail`
-    const openBtnQuery = `#${e.currentTarget.dataset.card} .detail-open-btn`
-    this.element.querySelector(detailQuery).classList.toggle('d-none')
-    this.element.querySelector(openBtnQuery).classList.toggle('d-none')
+  static targets = ["content", "button"]
+
+  toggle() {
+    this.contentTarget.classList.toggle("d-none")
+    this.buttonTarget.classList.toggle("d-none")
   }
 }
